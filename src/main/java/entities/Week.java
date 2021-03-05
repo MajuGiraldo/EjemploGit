018617fa9daab820a023c0fabf35e01bbe60dad0
Maria Juliana Giraldo Naranjo <mariagina@unisabana.edu.co>
2021-03-05 15:14:17 -0500

    este es un ejemplo Zz Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/entities/Week.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Week {
+
+    private Plan plan;
+    private List<Session> sessions;
+
+    public Week() {
+        this.sessions = new ArrayList<>();
+    }
+
+    public void registerSession(Session session) {
+        this.sessions.add(session);
+    }
+
+    public void assignPlan(Plan plan) {
+        this.plan = plan;
+    }
+}
